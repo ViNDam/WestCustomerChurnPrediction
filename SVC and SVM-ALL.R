a1aaf15c1fc77a018ee25d2ba045c8ba0b455aa6
Vi Dam <105737426+ViNDam@users.noreply.github.com>
2022-10-13 13:02:32 -0400

    Update SVC and SVM-ALL.R

--- SVC and SVM-ALL.R
+++ SVC and SVM-ALL.R
@@ -2,6 +2,108 @@
 # Author: Vi Dam
 # Version: 2018 05 15
 ###########################
+
+# ------------------------ RANDOM FOREST ------------------------
+library(DMwR)
+library(randomForest)
+library(pROC)
+
+setwd("C:/Users/vdam/Project Data")
+
+# Load csv file for clean data
+data = read.csv("Clean_Data_Fri.csv")
+
+set.seed(1029)
+
+# Set some of the data as a factor
+data[,c("total_accounts","Chief_Executive_Officer_Gender_C","Global_Ultimate_Indicator","Site_Status" ,"Owns_Rents_Code","Manufacturing_Indicator","Location_Type","Public_Private_Indicator","Small_Business_Indicator", "Minority_Owned_Indicator","churned")] <- lapply(data[,c("total_accounts","Chief_Executive_Officer_Gender_C","Global_Ultimate_Indicator","Site_Status" ,"Owns_Rents_Code","Manufacturing_Indicator","Location_Type","Public_Private_Indicator","Small_Business_Indicator", "Minority_Owned_Indicator","churned")], factor)
+
+# Divide Data
+n = nrow(data)
+sample_size = 0.7*n
+
+train = sample(n, sample_size)
+test = -train
+
+training.set = data[train,]
+test.set = data[-train,]
+
+# Use Smote to create balance data
+as.data.frame(table(training.set$churned))
+balanced.data <- SMOTE(churned ~., training.set, perc.over = 950, k = 50, perc.under =165)
+as.data.frame(table(balanced.data$churned))
+
+# Random Forest model
+rf.project = randomForest(churned ~ ., data = balanced.data, importance = TRUE, ntree=1000)
+rf.pred = predict(rf.project, newdata = test.set)
+
+# Confusion Matrix
+library(caret)
+confusionMatrix(rf.pred, test.set$churned, positive = "1")
+
+# Draw Roc and get AUC
+rf.prob = predict(rf.project, newdata = test.set, type = "prob")[,2]
+roc(test.set$churned, rf.prob, plot = TRUE, print.auc = TRUE, legacy.axes = TRUE)
+
+
+#-------------------------BOOSTING MODEL-------------------------
+library(gbm)
+library(DMwR)
+
+setwd("C:/Users/vdam/Project Data")
+
+# Load CSV file for cleaned Data
+data = read.csv("Clean_Data_Fri.csv")
+
+# Set data as factor
+data[,c("Chief_Executive_Officer_Gender_C","Global_Ultimate_Indicator","Site_Status" ,"Owns_Rents_Code","Manufacturing_Indicator","Location_Type","Public_Private_Indicator","Small_Business_Indicator", "Minority_Owned_Indicator","churned")] <- lapply(data[,c("Chief_Executive_Officer_Gender_C","Global_Ultimate_Indicator","Site_Status" ,"Owns_Rents_Code","Manufacturing_Indicator","Location_Type","Public_Private_Indicator","Small_Business_Indicator", "Minority_Owned_Indicator","churned")], factor)
+
+# Divide data
+set.seed(1300)
+n = nrow(data)
+sample_size = 0.7*n
+
+train = sample(n, sample_size)
+test = -train
+
+# Set new data frame 
+data.gbm = data
+
+# Change data to factor
+data.gbm[,c("total_accounts","Chief_Executive_Officer_Gender_C","Global_Ultimate_Indicator","Site_Status" ,"Owns_Rents_Code","Manufacturing_Indicator","Location_Type","Public_Private_Indicator","Small_Business_Indicator", "Minority_Owned_Indicator","churned")] <- lapply(data.gbm[,c("total_accounts","Chief_Executive_Officer_Gender_C","Global_Ultimate_Indicator","Site_Status" ,"Owns_Rents_Code","Manufacturing_Indicator","Location_Type","Public_Private_Indicator","Small_Business_Indicator", "Minority_Owned_Indicator","churned")], factor)
+
+# Changed churned to characters
+data.gbm$churned = ifelse(data.gbm$churned == 0, "0", "1")
+
+# Data frames for training and test
+training.set = data.gbm[train,]
+test.set = data.gbm[-train,]
+training.set$churned = factor(training.set$churned)
+
+
+# Smote model to increase churned value-------------------------
+as.data.frame(table(training.set$churned))
+balanced.data <- SMOTE(churned ~., training.set, perc.over = 950, k = 50, perc.under =165)
+
+as.data.frame(table(balanced.data$churned))
+
+# Convert back to character after Smote
+balanced.data$churned = ifelse(balanced.data$churned == 0, "0", "1")
+
+# Create GBM model 
+gbm.project = gbm(churned ~ ., data = balanced.data, n.trees = 1000, cv.folds = 5, shrinkage = 0.01)
+
+# Data Prediction
+gbm.probs = predict( gbm.project, newdata = test.set, n.trees = 1000, type = "response")
+gbm.pred = rep('0', length(gbm.probs))
+gbm.pred[gbm.probs > 0.5] = '1'
+gbm.pred = factor(gbm.pred)
+
+# Create Confusion Matrix
+confusionMatrix(gbm.pred, factor(test.set$churned), positive = "1")
+roc(test.set$churned, gbm.probs, plot = TRUE, print.auc = TRUE, legacy.axes = TRUE)
+
+# ------------------------ SUPPORT VECTOR CLASSIFIER SVM ------------------------
 library(e1071) #package for SVM in R
 library(caret)
 library(pROC)
@@ -9,7 +111,6 @@ library(forcats)
 
 setwd("/Users/vdam/Project Data/")
 data <- read.csv("Clean_Data_Fri.csv")
-#data <- adjusted4
 
 data[,c("Owns_Rents_Code","Manufacturing_Indicator","Legal_Status_Code","Population_Code","Hierarchy_Code","Revenue_Range","churned")] <- 
   lapply(data[,c("Owns_Rents_Code","Manufacturing_Indicator","Legal_Status_Code","Population_Code","Hierarchy_Code","Revenue_Range","churned")], factor)
@@ -25,7 +126,7 @@ test = -train
 training.set = data[train,]
 test.set = data[-train,]
 
-# Balance Dataset
+# Balance Dataset------------------------
 # balance variables so predicted values are even
 set.seed(1029)
 library(DMwR)
@@ -39,7 +140,7 @@ as.data.frame(table(balanced.data$churned))
 
 training.set <- balanced.data
 
-# Support Vector Classifier
+# Model
 tune.settings = tune.control(sampling = "cross", cross = 2)
 
 tune.svc = tune(svm, churned ~ ., data = training.set, kernel = "linear", 
@@ -54,10 +155,12 @@ best.model.pred = predict(best.model, newdata = test.set)
 best.model.prob = predict(best.model, newdata = test.set, probability = TRUE)
 best.model.prob = attr(best.model.prob, "probabilities")[,2]
 
+# Confusion Matrix -----
 confusionMatrix(best.model.pred, test.set$churned, positive = "1")
 #roc(test.set$churned, best.model.prob, plot = TRUE, print.auc = TRUE, legacy.axes = TRUE)
 save.image(file=paste("Linear-", po, "-", pu,"-cost-",cst,".RData", sep=""))
-#---------
+
+# ROC curve -----
 png("Rplots.png")
 plot(roc(test.set$churned, best.model.prob, plot = TRUE, print.auc = TRUE, legacy.axes = TRUE))
 dev.off()
@@ -71,7 +174,7 @@ confusionMatrix(as.factor(pred0.3), test.set$churned, positive = "1")
 plot(roc(test.set$churned, as.numeric(pred0.3), plot = TRUE, print.auc = TRUE, legacy.axes = TRUE))
 dev.off()
 
-# ---------------SVM Radial
+# ------------------------SUPPORT VECTOR MACHINE SVM Radial------------------------
 setwd("/Users/ViDam/Project Data/")
 
 library(e1071) #package for SVM in R
@@ -98,6 +201,7 @@ set.seed(1029)
 library(DMwR)
 
 as.data.frame(table(training.set$churned))
+
 # change po and pu for other data set: 100-900, 1000-200, 200-300, 500-120, -100
 print("balanced.data <- SMOTE(churned ~., training.set, perc.over = 100, k = 5, perc.under = 900)")
 balanced.data <- SMOTE(churned ~., training.set, perc.over = 100, k = 5, perc.under = 900)
@@ -124,14 +228,12 @@ best.model.svm.pred = predict(best.model.svm, newdata = test.set)
 best.model.svm.prob = predict(best.model.svm, newdata = test.set, probability = TRUE)
 best.model.svm.prob = attr(best.model.svm.prob, "probabilities")[,2]
 
+# confusion matrix -----
 confusionMatrix(best.model.svm.pred, test.set$churned, positive = "1")
-
-
-
 
 save.image(file="SVMRadial-100-900.RData")
 
-# ------SVMPoly
+# ------------------------SUPPORT VECTOR MACHINE SVMPoly------------------------
 library(e1071) #package for SVM in R
 library(caret)
 library(pROC)
@@ -180,14 +282,12 @@ best.model.svm.pred.poly = predict(best.model.svm.poly, newdata = test.set)
 best.model.svm.prob.poly = predict(best.model.svm.poly, newdata = test.set, probability = TRUE)
 best.model.svm.prob.poly = attr(best.model.svm.prob.poly, "probabilities")[,2]
 
+#confusion matrix -----
 confusionMatrix(best.model.svm.pred.poly, test.set$churned, positive = "1")
-
-
-
 
 save.image(file="SVMPoly-200-300.RData")
 
-# ----- SVMSigmoid
+# ------------------------ SUPPORT VECTOR MACHINE SVMSigmoid------------------------
 library(e1071) #package for SVM in R
 library(caret)
 library(pROC)
@@ -219,9 +319,6 @@ as.data.frame(table(balanced.data$churned))
 
 training.set <- balanced.data
 
-#training.set <- read.csv("training.csv")
-#test.set <- read.csv("testing.csv")
-
 tune.settings = tune.control(sampling = "cross", cross = 2)
 tune.svm.sig = tune(svm, churned ~ ., data = training.set, kernel = "sigmoid",
                     ranges=list(cost=c(0.01,1,10),d=c(3,4),
@@ -241,19 +338,22 @@ best.model.svm.pred.sig = predict(best.model.svm.sig, newdata = test.set)
 best.model.svm.prob.sig = predict(best.model.svm.sig, newdata = test.set, probability = TRUE)
 best.model.svm.prob.sig = attr(best.model.svm.prob.sig, "probabilities")[,2]
 
+# confusion Matrix -----
 confusionMatrix(best.model.svm.pred.sig, test.set$churned, positive = "1")
 save.image(file="SVMSigmoid-500-120.Rdata")
-#------
+# ROC curve -----
 png("Rplots.png")
 plot(roc(test.set$churned, best.model.svm.prob.sig, plot = TRUE, print.auc = TRUE, legacy.axes = TRUE))
 dev.off()
 
+# best model-----
 pred0.3 <- best.model.svm.prob.sig
 pred0.3[pred0.3 > 0.3] <- 1
 pred0.3[pred0.3 <= 0.3] <- 0
 
+# confusion Matrix -----
 confusionMatrix(as.factor(pred0.3), test.set$churned, positive = "1")
-
+# ROC curve -----
 plot(roc(test.set$churned, as.numeric(pred0.3), plot = TRUE, print.auc = TRUE, legacy.axes = TRUE))
 dev.off()
 
